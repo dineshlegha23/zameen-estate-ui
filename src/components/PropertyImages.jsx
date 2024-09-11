84c60fbff03c23460fabcbc16c98de10e4a19f5b
Dinesh <dineshlegha23@gmail.com>
2024-09-11 20:54:27 +0530

    adjusted property images to the bottom

--- src/components/PropertyImages.jsx
+++ src/components/PropertyImages.jsx
@@ -2,18 +2,18 @@ import React from "react";
 
 const PropertyImages = ({ singlePostData }) => {
   return (
-    <div className="flex gap-5 w-full">
+    <div className="flex sm:flex-col gap-5 w-full">
       <img
         src={singlePostData.images[0]}
-        className="w-[73%] sm:w-[70%] h-[350px] sm:h-[300px] object-cover rounded-lg"
+        className="w-[73%] sm:w-full h-[350px] sm:h-[300px] object-cover rounded-lg"
         alt="image"
       />
-      <div className="flex w-full flex-col gap-5">
+      <div className="flex sm:flex-row w-full flex-col gap-5 sm:gap-2">
         {singlePostData.images.slice(1).map((image) => (
           <img
             key={image}
             src={image}
-            className="w-full min-h-[103px] max-h-[103px] sm:max-h-[87px] sm:min-h-[87px] rounded-lg object-cover"
+            className="w-full min-h-[103px] max-h-[103px] sm:max-h-[87px] sm:min-h-[87px] sm:w-[30vw] rounded-lg object-cover"
           />
         ))}
       </div>
